1112e937ec50125e2a812342ebcc277961c4d194
Apple-Kong <kongthn@gmail.com>
2022-06-04 18:43:15 +0900

    removeDuplicate

--- Filtering Operators.playground/Contents.swift
+++ Filtering Operators.playground/Contents.swift
@@ -21,3 +21,16 @@ example(of: "filter") {
         .store(in: &subscriptions)
 }
 
+//MARK: - removeDuplicates
+example(of: "remove duplicates") {
+    var subscriptions = [AnyCancellable]()
+    
+    let words = "어이 어이 거기! 앞은 좀 좀 보고 다니라고~"
+        .components(separatedBy: " ")
+        .publisher
+    
+    words
+        .removeDuplicates()
+        .sink(receiveValue: { print($0)})
+        .store(in: &subscriptions)
+}
